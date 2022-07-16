2aba336ca6e9aa985b62528688130da7b10e91b5
Marc <marc@highcard.dev>
2022-07-16 21:20:58 +0200

    feat: use version instead of commit hash for latest.json

--- find-changed-scrolls.sh
+++ find-changed-scrolls.sh
@@ -2,26 +2,24 @@
 #changedScrolls=$(git diff --dirstat=files,0,cumulative HEAD~1 scrolls/ | awk '{print $2}' | cut -d'/' -f2 | uniq | grep -v -e '^$')
 
 
-if [ -f latest.json ]; then
-    mv latest.json old-latest.json
-else
+if [ ! -f old-latest.json ]; then
+    echo "creating old-latest.json"
     echo "{}" > old-latest.json
 fi
 
 echo "{}" > latest.json
 
 for d in scrolls/*/ ; do
-    ref=$(git log --format="%H" -n 1 -- $d)
+    version=$(cat $d/scroll.json | jq -r '.version')
+    echo $version
     scrollName=$(echo "$d" | cut -d'/' -f2)
-    jq '."'"$scrollName"'" = "'"$ref"'"' latest.json|sponge latest.json
+    jq '."'"$scrollName"'" = "'"$version"'"' latest.json|sponge latest.json
 done
 
 echo "scrolls that changed"
 changedScrolls=$(node get-diff.js)
 
-ref=$(git rev-parse HEAD)
 for s in $changedScrolls ; do
     echo $s
-    jq '.ref = "'"$ref"'"' scrolls/$s/scroll.json|sponge scrolls/$s/scroll.json
     tar -czvf scrolls/$s.tar.gz -C scrolls/$s .
 done
