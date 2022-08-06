f234a2c41aa5e5447c58e5561dd8ca001106bb4f
Marc <marc@highcard.dev>
2022-08-06 18:21:28 +0200

    chore: skip update when scroll lock not present

--- scrolls/mc-sp@1.17/update.sh
+++ scrolls/mc-sp@1.17/update.sh
@@ -1,12 +1,18 @@
 #default update script
-versions=$(find * -maxdepth 0 -type d | sort --version-sort)
-current=$(cat scroll-lock.json | jq -r .version)
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-for version in $versions
-do
+if [ ! -f "$SCRIPTPATH/scroll-lock.json" ]; then
+	echo "Scroll lock not found. Skipping update"
+	exit 0
+fi
 
+versionsDirs=$(find update/* -maxdepth 0 -type d | sort --version-sort)
+current=$(cat $SCRIPTPATH/scroll-lock.json | jq -r .version)
+
+for versionsDir in $versionsDirs
+do
+	version=$(basename $versionsDir)
 	if [ ! "$(printf '%s\n' "$version" "$current" | sort -V | head -n1)" = "$version" ] ;
 	then
 		if [ -f "$version/update.sh" ]; then
