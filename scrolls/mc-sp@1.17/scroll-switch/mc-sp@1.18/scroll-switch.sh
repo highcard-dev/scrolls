c7ddf57516f98d20766d54ad91f048cf41be13d1
Marc <marc@highcard.dev>
2022-08-08 22:36:12 +0200

    fix(mc-sp@1.18): fix download url

--- scrolls/mc-sp@1.17/scroll-switch/mc-sp@1.18/scroll-switch.sh
+++ scrolls/mc-sp@1.17/scroll-switch/mc-sp@1.18/scroll-switch.sh
@@ -5,8 +5,9 @@ if ["$SCROLL_DIR" == ""]; then
     SCROLL_DIR="./.scroll"
 fi
 
-cp $NEW_SCROLL_DIR/scroll.json $SCROLL_DIR/scroll.json
 
 wget -O spigot-new.jar https://download.getbukkit.org/spigot/spigot-1.19.jar
 rm spigot.jar
 mv spigot-new.jar spigot.jar
+
+cp $NEW_SCROLL_DIR/scroll.json $SCROLL_DIR/scroll.json
