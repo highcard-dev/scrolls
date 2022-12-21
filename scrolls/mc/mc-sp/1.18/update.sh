5a9ba8d68afff151deb040f5b33c0c65a3873fa0
Marc <marc@highcard.dev>
2022-12-21 22:48:42 +0100

    chore: dir check update.sh

--- scrolls/mc/mc-sp/1.18/update.sh
+++ scrolls/mc/mc-sp/1.18/update.sh
@@ -7,22 +7,28 @@ if [ ! -f "$SCRIPTPATH/scroll-lock.json" ]; then
 	exit 0
 fi
 
-versionsDirs=$(find $SCRIPTPATH/update/* -maxdepth 0 -type d | sort --version-sort)
-current=$(cat $SCRIPTPATH/scroll-lock.json | jq -r .scroll_version)
+if [ -z "$(ls $SCRIPTPATH/update)" ]; then
+   echo "Update directory is empty. Skipping update"
+else
+	versionsDirs=$(find $SCRIPTPATH/update/* -maxdepth 0 -type d | sort --version-sort)
+	current=$(cat $SCRIPTPATH/scroll-lock.json | jq -r .scroll_version)
 
-for versionsDir in $versionsDirs
-do
-	version=$(basename $versionsDir)
-	if [ ! "$(printf '%s\n' "$version" "$current" | sort -V | head -n1)" = "$version" ] ;
-	then
-		echo "$versionsDir/update.sh"
-		if [ -f "$versionsDir/update.sh" ]; then
-			sh $versionsDir/update.sh
-		else
-			echo "Warning: update $version has no update.sh... skipping"
+	for versionsDir in $versionsDirs
+	do
+		version=$(basename $versionsDir)
+		if [ ! "$(printf '%s\n' "$version" "$current" | sort -V | head -n1)" = "$version" ] ;
+		then
+			echo "$versionsDir/update.sh"
+			if [ -f "$versionsDir/update.sh" ]; then
+				sh $versionsDir/update.sh
+			else
+				echo "Warning: update $version has no update.sh... skipping"
+			fi
 		fi
-	fi
-done
+	done
+fi
+
+
 
 LATEST_VERSION=$(cat $SCRIPTPATH/scroll.json | jq -r .version)
 jq --arg LV "$LATEST_VERSION" -r '.scroll_version = $LV' $SCRIPTPATH/scroll-lock.json | sponge $SCRIPTPATH/scroll-lock.json 
