ef9c88e2d72498656aed8841bacad609b8a68e42
Marc <marc@highcard.dev>
2023-01-15 23:32:44 +0100

    feat: dockerfiles

---
+++ .docker/prepare-switch-scroll.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+CURRENT_SCROLL=$(cat $SCROLL_DIR/scroll.yaml | yq .name)
+
+echo "Current scroll: $CURRENT_SCROLL"
+
+if [ "$CURRENT_SCROLL" != "$SCROLL" ]; then
+    druid run switch-scroll $SCROLL
+    rm scroll-lock.json
+else
+    echo "Already on $SCROLL. Nothing to do."
+fi
