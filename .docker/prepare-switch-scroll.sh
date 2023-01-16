e2d43c795c46736f3fadd2a3feb7c1b280288692
Marc <marc@highcard.dev>
2023-01-16 16:17:48 +0100

    chore: adjust prepare-switch-scroll script

--- .docker/prepare-switch-scroll.sh
+++ .docker/prepare-switch-scroll.sh
@@ -3,9 +3,11 @@ CURRENT_SCROLL=$(cat $SCROLL_DIR/scroll.yaml | yq .name)
 
 echo "Current scroll: $CURRENT_SCROLL"
 
-if [ "$CURRENT_SCROLL" != "$SCROLL" ]; then
-    druid run switch-scroll $SCROLL
+DESIRED_SCROLL="$SCROLL@$SCROLL_VERSION"
+
+if [ "$CURRENT_SCROLL" != "$DESIRED_SCROLL" ]; then
+    druid run switch-scroll $DESIRED_SCROLL
     rm $SCROLL_DIR/scroll-lock.json
 else
-    echo "Already on $SCROLL. Nothing to do."
+    echo "Already on $CURRENT_SCROLL. Nothing to do."
 fi
