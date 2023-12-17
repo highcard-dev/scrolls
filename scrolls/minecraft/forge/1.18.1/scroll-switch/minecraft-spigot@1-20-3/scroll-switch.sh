317f95649d3b6188ddaad9457872bca3392d2e48
Marc <marc@highcard.dev>
2023-12-17 21:35:30 +0100

    feat: minecraft forge

---
+++ scrolls/minecraft/forge/1.18.1/scroll-switch/minecraft-spigot@1-20-3/scroll-switch.sh
@@ -0,0 +1,4 @@
+wget -O forge-installer-new.jar https://s3.eu-central-1.wasabisys.com/druid-scroll-artifacts/minecraft/forge/forge-1.20.3.jar
+
+java -jar forge-installer-new.jar --installServer
+rm forge-installer-new.jar
