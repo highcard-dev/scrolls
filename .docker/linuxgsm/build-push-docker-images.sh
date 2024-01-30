e739d48622c1f351bf65b4186df5a21721f2f935
Marc <marc@highcard.dev>
2024-01-30 20:48:01 +0100

    chore: add option to build single linux gsm file

--- .docker/linuxgsm/build-push-docker-images.sh
+++ .docker/linuxgsm/build-push-docker-images.sh
@@ -2,11 +2,19 @@
 set -e
 curl -O "https://raw.githubusercontent.com/GameServerManagers/LinuxGSM/master/lgsm/data/serverlist.csv"
 
+#optional shortname to build only one image
+wanted_image=$1
+
 while read line; do
   export shortname=$(echo "$line" | awk -F, '{ print $1 }')
   export servername=$(echo "$line" | awk -F, '{ print $2 }')
   export gamename=$(echo "$line" | awk -F, '{ print $3 }')
   export distro=$(echo "$line" | awk -F, '{ print $4 }')
+  
+  if [ -n "$wanted_image" ] && [ "$wanted_image" != "$shortname" ]; then
+    continue
+  fi
+
   echo "Generating Dockerfile.${shortname} (${gamename})"
   echo "shortname ${shortname}"
   docker build -f Dockerfile . --build-arg SHORTNAME="${shortname}" --build-arg SERVERNAME="${servername}" --build-arg GAMENAME="${gamename}" --build-arg DISTRO="${distro}" -t "highcard/druidd-lgsm:${shortname}"
