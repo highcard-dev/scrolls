4f350596265ea203d9d6f5181d7cc36ec50de934
Marc Schottstädt <marc.schottstaedt@gmail.com>
2025-01-28 00:14:17 +0100

    chore: prebuild without local docker mounts

--- scripts/prebuild/prebuild.sh
+++ scripts/prebuild/prebuild.sh
@@ -9,27 +9,22 @@ export PRESIGN_OBJECT_KEY=lgsm/${TAG}-snapshot-latest.tar.gz
 PRESIGNED_URL=$(cd scripts/presign/ && go run main.go)
 
 
-rm -rf tmp-prebuild
-mkdir -p tmp-prebuild/deployment
-chmod 777 -R tmp-prebuild
-
-wget -O tmp-prebuild/druid-install-command.sh https://github.com/highcard-dev/druid-cli/releases/latest/download/druid-install-command.sh
-chmod +x tmp-prebuild/druid-install-command.sh
+docker run --rm -v build-folder:/app/resources bash sh -c 'wget -O /app/resources/druid-install-command.sh https://github.com/highcard-dev/druid-cli/releases/latest/download/druid-install-command.sh && mkdir /app/resources/deployment && chmod +x /app/resources/druid-install-command.sh'
 
 DOCKER_ENTRYPOINT=/app/resources/druid-install-command.sh
 
-echo "Running docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v ./tmp-prebuild:/app/resources --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druidd-lgsm:$TAG registry pull artifacts.druid.gg/druid-team/scroll-lgsm:${TAG}server" 
+echo "Running docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v build-folder:/app/resources --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druidd-lgsm:$TAG registry pull artifacts.druid.gg/druid-team/scroll-lgsm:${TAG}server" 
 
-docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v ./tmp-prebuild:/app/resources --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druidd-lgsm:$TAG registry pull artifacts.druid.gg/druid-team/scroll-lgsm:${TAG}server
+docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v build-folder:/app/resources --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druidd-lgsm:$TAG registry pull artifacts.druid.gg/druid-team/scroll-lgsm:${TAG}server
 
-echo "Running docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v ./tmp-prebuild:/app/resources --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druidd-lgsm:$TAG run install"
+echo "Running docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v build-folder:/app/resources --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druidd-lgsm:$TAG run install"
 
-docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v ./tmp-prebuild:/app/resources --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druidd-lgsm:$TAG run install
+docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v build-folder:/app/resources --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druidd-lgsm:$TAG run install
 
 echo "Prebuild done"
 
-echo "Running docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v ./tmp-prebuild:/app/resources --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druidd-lgsm:$TAG backup $PRESIGNED_URL"
-docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v ./tmp-prebuild:/app/resources --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druidd-lgsm:$TAG backup $PRESIGNED_URL
+echo "Running docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v build-folder:/app/resources --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druidd-lgsm:$TAG backup $PRESIGNED_URL"
+docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v build-folder:/app/resources --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druidd-lgsm:$TAG backup $PRESIGNED_URL
 echo "Prebuild uploaded"
 
 
