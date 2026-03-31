ae9c73b3aeae730a0b32def1dae69ea42efa04b7
Marc <14913675+MarcStdt@users.noreply.github.com>
2026-03-31 16:43:35 +0200

    Scroll v2 (#22)
    * feat: scroll v2
    
    * fix: pr
    
    * chore: sudo
    
    * chore: use strict validation
    
    * chore: pass strict validation
    
    * chore: delete init-files
    
    * chore: remove more init files
    
    * chore: remove artifacts.json
    
    * chore: remove scroll-switch

--- scrolls/minecraft/papermc/1.17/scroll-switch/minecraft-paper@1-17-1/scroll-switch.sh
+++
@@ -1,3 +0,0 @@
-wget -O paper-new.jar https://fsn1.your-objectstorage.com/druid-deployment-assets/minecraft/papermc/paper-1.17.1.jar
-rm paper.jar
-mv paper-new.jar paper.jar
--- scrolls/minecraft/papermc/1.19/scroll-switch/minecraft-paper@1-21-4/scroll-switch.sh
+++
@@ -1,3 +0,0 @@
-wget -O paper-new.jar https://fsn1.your-objectstorage.com/druid-deployment-assets/minecraft/papermc/paper-1.21.4.jar
-rm paper.jar
-mv paper-new.jar paper.jar
