448f785b1e064d096826368944a41ea8a43a1ef7
Marc Schottstädt <marc.schottstaedt@gmail.com>
2026-04-03 18:31:25 +0200

    fix: prebuild ci

--- scripts/prebuild/prebuild.sh
+++ scripts/prebuild/prebuild.sh
@@ -4,8 +4,6 @@ set -e
 TAG=$1
 echo "Tag: $TAG"
 
-PRESIGN_OBJECT_KEY=lgsm/${TAG}-snapshot-latest.tar.gz
-
 TMP_VOLUME_NAME=lgsm-prebuild-$(date +%s)
 
 docker volume rm $TMP_VOLUME_NAME || true
@@ -17,7 +15,7 @@ docker run --rm -v $TMP_VOLUME_NAME:/app/resources bash sh -c 'wget -O /app/reso
 DOCKER_ENTRYPOINT=/app/resources/druid-install-command.sh
 
 echo "Login to registry"
-docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v $TMP_VOLUME_NAME:/app/resources -v $DRUID_CONFIG:$HOME/druid.yml --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druid:latest-nix-steamcmd registry login --host ${SCROLL_REGISTRY_HOST} --user '${SCROLL_REGISTRY_USER}' --password ${SCROLL_REGISTRY_PASSWORD}
+docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v $TMP_VOLUME_NAME:/app/resources -v $DRUID_CONFIG:$HOME/druid.yml --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druid:latest-nix-steamcmd registry login --host "${SCROLL_REGISTRY_HOST}" --user "${SCROLL_REGISTRY_USER}" --password "${SCROLL_REGISTRY_PASSWORD}"
 
 echo "Pulling scroll"
 docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v $TMP_VOLUME_NAME:/app/resources --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druid:latest-nix-steamcmd registry pull artifacts.druid.gg/druid-team/scroll-lgsm:${TAG}server
