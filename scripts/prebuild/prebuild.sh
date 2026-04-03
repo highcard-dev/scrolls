0d02cb92fcd0c9096ec7ffb12fde095b5cc647e1
Marc Schottstädt <marc.schottstaedt@gmail.com>
2026-04-03 15:05:15 +0200

    chore: prebuild login

--- scripts/prebuild/prebuild.sh
+++ scripts/prebuild/prebuild.sh
@@ -10,10 +10,14 @@ TMP_VOLUME_NAME=lgsm-prebuild-$(date +%s)
 
 docker volume rm $TMP_VOLUME_NAME || true
 
+DRUID_CONFIG=$(mktemp)
+
 docker run --rm -v $TMP_VOLUME_NAME:/app/resources bash sh -c 'wget -O /app/resources/druid-install-command.sh https://github.com/highcard-dev/druid-cli/releases/latest/download/druid-install-command.sh && mkdir -p /app/resources/deployment && chmod +x /app/resources/druid-install-command.sh && chown 1000:1000 -R /app/resources/'
 
 DOCKER_ENTRYPOINT=/app/resources/druid-install-command.sh
 
+echo "Login to registry"
+docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v $TMP_VOLUME_NAME:/app/resources -v $DRUID_CONFIG:$HOME/druid.yml --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druid:latest-nix-steamcmd registry login --host ${SCROLL_REGISTRY_HOST} --user '${SCROLL_REGISTRY_USER}' --password ${SCROLL_REGISTRY_PASSWORD}
 
 echo "Pulling scroll"
 docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v $TMP_VOLUME_NAME:/app/resources --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druid:latest-nix-steamcmd registry pull artifacts.druid.gg/druid-team/scroll-lgsm:${TAG}server
@@ -21,9 +25,9 @@ docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v $TMP_VOLUME_NAME:/app/resourc
 echo "Running scroll install script"
 docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v $TMP_VOLUME_NAME:/app/resources --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druid:latest-nix-steamcmd run install
 
-echo "Creating archive and uploading to s3"
-docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v $TMP_VOLUME_NAME:/app/resources --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druid:latest-nix-steamcmd backup $PRESIGN_OBJECT_KEY --s3-access-key $PRESIGN_ACCESS_KEY --s3-secret-key $PRESIGN_SECRET_KEY --s3-bucket $PRESIGN_BUCKET_NAME --s3-endpoint $PRESIGN_S3_ENDPOINT $BACKUP_ADDITIONAL_ARGS
+echo "Pushing scroll to registry"
+docker run --entrypoint $DOCKER_ENTRYPOINT --rm -v $TMP_VOLUME_NAME:/app/resources -v $DRUID_CONFIG:$HOME/druid.yml --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druid:latest-nix-steamcmd registry push artifacts.druid.gg/druid-team/scroll-lgsm:${TAG}server-prebuild
 echo "Prebuild uploaded"
 
-
+rm -f $DRUID_CONFIG
 docker volume rm $TMP_VOLUME_NAME || true
