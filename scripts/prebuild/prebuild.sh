ad9004b1942a43d48cb44585667a56e8df3aa7f5
Marc Schottstädt <marc.schottstaedt@gmail.com>
2026-05-28 22:41:20 +0200

    fix(ci): correct pinned druid installer url

--- scripts/prebuild/prebuild.sh
+++ scripts/prebuild/prebuild.sh
@@ -11,7 +11,7 @@ TMP_VOLUME_NAME=lgsm-prebuild-$(date +%s)
 
 docker volume rm $TMP_VOLUME_NAME || true
 
-docker run --rm -v $TMP_VOLUME_NAME:/app/resources -e CHANNEL="$DRUID_CLI_VERSION" bash sh -c 'wget -O /app/resources/druid-install-command.sh "https://github.com/highcard-dev/druid-cli/releases/${CHANNEL}/download/druid-install-command.sh" && mkdir -p /app/resources/deployment && chmod +x /app/resources/druid-install-command.sh && chown 1000:1000 -R /app/resources/'
+docker run --rm -v $TMP_VOLUME_NAME:/app/resources -e CHANNEL="$DRUID_CLI_VERSION" bash sh -c 'wget -O /app/resources/druid-install-command.sh "https://github.com/highcard-dev/druid-cli/releases/download/${CHANNEL}/druid-install-command.sh" && mkdir -p /app/resources/deployment && chmod +x /app/resources/druid-install-command.sh && chown 1000:1000 -R /app/resources/'
 
 echo "Pulling scroll"
 docker run --rm -v $TMP_VOLUME_NAME:/app/resources --entrypoint /app/resources/druid-install-command.sh -w /app/resources/deployment artifacts.druid.gg/druid-team/druid:latest-nix-steamcmd registry pull artifacts.druid.gg/druid-team/scroll-lgsm:${TAG}server
